83a81fd9aa5eb6f81b13a5a3815335f2097f139a
Vraj1103 <svraj073@gmail.com>
2025-05-10 18:18:52 +0530

    docker file init

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
